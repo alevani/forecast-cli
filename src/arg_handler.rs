8089afec846fdf28271c1104aaff25c693d8eb2e
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-11-18 09:48:26 +0100

    Rewrite argument parser with CLAP's derive feature

--- src/arg_handler.rs
+++ src/arg_handler.rs
@@ -1,31 +1,46 @@
-use clap::{value_t, App as CLAPApp, Arg};
+use clap::{Parser, Subcommand};
 
-pub fn get_values() -> (Option<String>, String) {
-    let args = CLAPApp::new("HOWTO")
-        .version("1.0")
-        .arg(
-            Arg::with_name("task")
-                .short("t")
-                .long("task")
-                .help("Task number e.g: T15115")
-                .value_name("Task number")
-                .takes_value(true)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("log")
-                .short("l")
-                .long("log")
-                .help("Number of hours to log e.g: 0h25m")
-                .value_name("Number of hour to log")
-                .takes_value(true)
-                .required(true),
-        )
-        .get_matches();
+/// Forecast CLI
+#[derive(Parser, Debug)]
+#[command(next_line_help = true)]
+pub struct LogArgs {
+    /// The command you wish to execute
+    #[command(subcommand)]
+    pub command: Command,
+}
 
-    // Quit the CLI if the value were not present
-    let task_number = value_t!(args, "task", String).ok();
-    let number_of_hours = value_t!(args, "log", String).unwrap_or_else(|e| e.exit());
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Login to Forecast with your Adapt Google account
+    Login,
 
-    (task_number, number_of_hours)
+    /// Logout from the CLI
+    Logout,
+
+    /// Undo your last time registration (only work after a log)
+    Undo,
+
+    /// Fetch a list of today's registered time
+    Today,
+
+    /// Log your time spent on a task
+    Log {
+        /// The time to register with the format <Number of hours>h<Optional: Number of minutes>. Examples: 2h or 2h45
+        time: String,
+
+        /// Task ID (e.g: T10019), will attempt to fetch the ID from your current git branch if not provided
+        #[arg(short, long)]
+        task: Option<String>,
+
+        /// A short message about what you did on the task
+        #[arg(short, long)]
+        message: Option<String>,
+    },
+
+    /// Open a task in your web browser
+    Open {
+        /// Task ID (e.g: T10019), will attempt to fetch the ID from your current git branch if not provided
+        #[arg(short, long)]
+        task: Option<String>,
+    },
 }
